f74d68217e81b8ddd3ce46bdd6cf86a8fee39b32
Aquarel <54329158+ThatAquarel@users.noreply.github.com>
2021-09-21 07:38:53 -0400

    Copy Source

--- src/content_script.tsx
+++ src/content_script.tsx
@@ -1,9 +1,12 @@
-chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
-  if (msg.color) {
-    console.log("Receive color = " + msg.color);
-    document.body.style.backgroundColor = msg.color;
-    sendResponse("Change color to " + msg.color);
-  } else {
-    sendResponse("Color message is none.");
-  }
-});
+let listening_questions = document.getElementsByClassName("ms-act-section-workbook-card-a-item");
+
+for (const question of listening_questions) {
+    let answers = question.children;
+    for (const answer of answers) {
+        let data_answer = answer.getAttribute("data-answer");
+        let text_color = ((data_answer === "True") ? "Green" : "Red")
+
+        answer.children[1].innerHTML +=
+            `<p style="color: ${text_color}; font-weight: bold;">${data_answer}</p>`
+    }
+}
